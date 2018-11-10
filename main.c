bff6a55e8e53733558a0f64fed8748fab36fb24c
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2018-11-10 12:41:27 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -32,7 +32,6 @@ void ADC_init() {
 }
 
 
-
 enum IR_EMITTER_STATES{IR_EMIT_START,IR_EMIT_INIT,IR_EMIT_PULSE,IR_EMIT_OFF}ir_emit_state;
 
 
@@ -42,30 +41,48 @@ void ir_emit_init(){
 
 void ir_emit_tick(){
 	switch(ir_emit_state){
+		
 		case IR_EMIT_START:
 		ir_emit_state = IR_EMIT_INIT;
 		break;
+		
 		case IR_EMIT_INIT:
 		ir_emit_state = IR_EMIT_PULSE;
 		break;
+		
 		case IR_EMIT_PULSE:
 		ir_emit_state = IR_EMIT_OFF;
 		break;
+		
 		case IR_EMIT_OFF:
 		ir_emit_state = IR_EMIT_PULSE;
+		break;
+
 		default:
+		ir_emit_state = IR_EMIT_START;
 		break;
 	}
 	switch(ir_emit_state){
 		case IR_EMIT_START:
 		break;
+		
 		case IR_EMIT_INIT:
 		break;
+		
 		case IR_EMIT_PULSE:
-		PORTA = (PORTA & 0xEF) | 0x10;
+		//PORTA = (PORTA & 0xEF) | 0x10;
+		//PORTA = (PORTA & 0xDF) | 0x20;
+		PORTA = (PORTA & 0xBF) | 0x40;
+		
+		//PORTA = (PORTA & 0x8F) | 0x70;
 		break;
+		
 		case IR_EMIT_OFF:
-		PORTA = (PORTA & 0xEF) | 0x10;
+		//PORTA = (PORTA & 0xEF) | 0x10;
+		//PORTA = (PORTA & 0xDF) | 0x20;
+		PORTA = (PORTA & 0xBF) | 0x40;
+		
+		//PORTA = (PORTA & 0x8F) | 0x70;
 		break;
 	}
 }
@@ -77,6 +94,10 @@ unsigned short right_reading;
 unsigned short x;
 
 enum IR_RECIEVER_STATES{IR_REC_START,IR_REC_INIT,RECIEVE}ir_rec_state;
+
+unsigned char C;
+unsigned char D;
+
 
 void ir_rec_init(){
 	ir_rec_state = IR_REC_START;
@@ -93,17 +114,55 @@ void ir_rec_tick(){
 		case RECIEVE:
 			ir_rec_state = RECIEVE;
 			break;
+		default:
+			ir_rec_state = IR_REC_START;
+			break;
 	}
 	switch(ir_rec_state){
 		case IR_REC_START:
 			break;
 		case IR_REC_INIT:
 			ADC_init();
+			ADMUX = (ADMUX & 0xF0)| 0x02; 
 			break;
 		case RECIEVE:
 			x = ADC;
+			front_reading = x;
+			//C  = (char)x;
+			//D = (char)(x>>8);
+			//PORTC = C;
+			//PORTD = D;
 			PORTC = (char)x;
 			PORTD = (char)(x >> 8);
+			break;
+	}
+}
+
+enum MOTOR_STATES{MOTOR_START,MOTOR_INIT,MOTOR}motor_state;
+void motor_init(){
+	motor_state = MOTOR_START;	
+}
+
+
+void motor_tick(){
+	switch(motor_state){
+		case MOTOR_START:
+			break;
+		case MOTOR_INIT:
+			break;
+		case MOTOR:
+			break;
+		default:
+			motor_state = MOTOR_START;
+			break;
+	}
+
+	switch(motor_state){
+		case MOTOR_START:
+			break;
+		case MOTOR_INIT:
+			break;
+		case MOTOR:
 			break;
 	}
 }
@@ -121,7 +180,14 @@ void ir_rec_task(){
 	ir_rec_init();
 	for(;;){
 		ir_rec_tick();
-		vTaskDelay(100);
+		vTaskDelay(50);
+	}
+}
+void motor_task(){
+	motor_init();
+	for(;;){
+		motor_tick();
+		vTaskDelay(100);	
 	}
 }
 
@@ -136,14 +202,21 @@ void StartSecPulse_2(unsigned portBASE_TYPE Priority)
 }
 
 
+void StartSecPulse_3(unsigned portBASE_TYPE Priority)
+{
+	xTaskCreate(motor_task,(signed portCHAR *)"motor_task",configMINIMAL_STACK_SIZE,NULL,Priority,NULL);
+}
 
 
 int main(void)
 {
 	DDRA = 0xF0;
+	//PORTA = 0x0F;
 
 	DDRC = 0xFF;
+	//PORTC = 0x00;
 	DDRD = 0xFF;
+	//PORTD = 0x00;
 
 
 	/*
@@ -151,11 +224,14 @@ int main(void)
 		PORTC = 0xFF;
 		PORTD = 0xFF;
 	}
-	*/
+	//*/
 
 
-	StartSecPulse(1);
+	StartSecPulse(2);
 	StartSecPulse_2(1);
+
+	//StartSecPulse_3(1);
+
 	vTaskStartScheduler();
 	
 
